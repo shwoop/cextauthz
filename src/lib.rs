8d0330bdb554333c08add04a5f71b7c2023d846f
shwoop <monkey.mambo.al.ferguson@gmail.com>
2026-05-21 20:56:41 +0200

    feat: First integration test

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,28 @@
-use proxy_wasm::traits::*;
-use proxy_wasm::types::*;
+#[cfg(target_arch = "wasm32")]
+mod wasm {
+    use proxy_wasm::traits::*;
+    use proxy_wasm::types::*;
 
-proxy_wasm::main! {{
-    proxy_wasm::set_log_level(LogLevel::Info);
-    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(NoopRoot) });
-}}
+    proxy_wasm::main! {{
+        proxy_wasm::set_log_level(LogLevel::Info);
+        proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(super::NoopRoot) });
+    }}
 
-struct NoopRoot;
+    impl Context for super::NoopRoot {}
 
-impl Context for NoopRoot {}
+    impl RootContext for super::NoopRoot {
+        fn get_type(&self) -> Option<ContextType> {
+            Some(ContextType::HttpContext)
+        }
 
-impl RootContext for NoopRoot {
-    fn get_type(&self) -> Option<ContextType> {
-        Some(ContextType::HttpContext)
+        fn create_http_context(&self, _: u32) -> Option<Box<dyn HttpContext>> {
+            Some(Box::new(super::NoopHttp))
+        }
     }
 
-    fn create_http_context(&self, _: u32) -> Option<Box<dyn HttpContext>> {
-        Some(Box::new(NoopHttp))
-    }
+    impl Context for super::NoopHttp {}
+    impl HttpContext for super::NoopHttp {}
 }
 
-struct NoopHttp;
-
-impl Context for NoopHttp {}
-impl HttpContext for NoopHttp {}
+pub struct NoopRoot;
+pub struct NoopHttp;
